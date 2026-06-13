13d74770a7e3e9fea56d2539ffc72e22013c164d
Oghenetejiri Enaibe <enaibs99@gmail.com>
2026-06-13 10:17:05 +0100

    initial commit

---
+++ intro.js
@@ -0,0 +1,5 @@
+const name = "Tejiri";
+const age = 26;
+const favoriteProgrammingLanguage = "JavaScript";
+
+console.log(`Hello, my name is ${name}. I am ${age} years old and my favorite programming language is ${favoriteProgrammingLanguage}.`);
